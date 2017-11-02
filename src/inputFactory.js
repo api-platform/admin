d37dec745f9b83ccf099789e3b4c7ed72fddbe64
Kévin Dunglas <dunglas@gmail.com>
2017-11-02 14:57:07 +0100

    Add support for to-many relations

--- src/inputFactory.js
+++ src/inputFactory.js
@@ -2,46 +2,80 @@ import {
   BooleanInput,
   DateInput,
   NumberInput,
+  ReferenceArrayInput,
   ReferenceInput,
+  SelectArrayInput,
   SelectInput,
   TextInput,
 } from 'admin-on-rest';
 import React from 'react';
 
 export default (field, options) => {
+  const props = {...field.inputProps};
   if (field.input) {
     return (
-      <field.input key={field.name} options={options} source={field.name} />
+      <field.input
+        key={field.name}
+        options={options}
+        source={field.name}
+        {...props}
+      />
     );
   }
 
+  if (!props.validate && field.required)
+    props.validate = value => (value ? undefined : 'Required');
+
   if (null !== field.reference) {
+    if (1 === field.maxCardinality) {
+      return (
+        <ReferenceInput
+          key={field.name}
+          label={field.name}
+          reference={field.reference.name}
+          source={field.name}
+          {...props}
+          allowEmpty>
+          <SelectInput optionText="id" />
+        </ReferenceInput>
+      );
+    }
+
     return (
-      <ReferenceInput
+      <ReferenceArrayInput
         key={field.name}
         label={field.name}
         reference={field.reference.name}
-        source={field.name}>
-        <SelectInput optionText="id" />
-      </ReferenceInput>
+        source={field.name}
+        {...props}
+        allowEmpty>
+        <SelectArrayInput optionText="id" />
+      </ReferenceArrayInput>
     );
   }
 
   switch (field.range) {
     case 'http://www.w3.org/2001/XMLSchema#integer':
-      return <NumberInput key={field.name} source={field.name} />;
+      return <NumberInput key={field.name} source={field.name} {...props} />;
 
     case 'http://www.w3.org/2001/XMLSchema#decimal':
-      return <NumberInput key={field.name} source={field.name} step="0.1" />;
+      return (
+        <NumberInput
+          key={field.name}
+          source={field.name}
+          step="0.1"
+          {...props}
+        />
+      );
 
     case 'http://www.w3.org/2001/XMLSchema#boolean':
-      return <BooleanInput key={field.name} source={field.name} />;
+      return <BooleanInput key={field.name} source={field.name} {...props} />;
 
     case 'http://www.w3.org/2001/XMLSchema#date':
     case 'http://www.w3.org/2001/XMLSchema#dateTime':
-      return <DateInput key={field.name} source={field.name} />;
+      return <DateInput key={field.name} source={field.name} {...props} />;
 
     default:
-      return <TextInput key={field.name} source={field.name} />;
+      return <TextInput key={field.name} source={field.name} {...props} />;
   }
 };
