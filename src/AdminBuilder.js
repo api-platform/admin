58bd4ee8f771e947d19954bbaf28feeaf15bd05b
Kévin Dunglas <dunglas@gmail.com>
2019-06-12 16:50:49 +0200

    use Query instead of withDataProvider

--- src/AdminBuilder.js
+++ src/AdminBuilder.js
@@ -1,53 +1,60 @@
-import React, { Component } from 'react';
-import {Admin, Resource, Loading, TranslationProvider, withDataProvider, ListGuesser, EditGuesser} from 'react-admin';
-import {createMuiTheme} from '@material-ui/core/styles';
+import React from "react";
+import {
+  Admin,
+  Resource,
+  Loading,
+  TranslationProvider,
+  Query,
+  ListGuesser,
+  EditGuesser
+} from "react-admin";
 import PropTypes from 'prop-types';
 import Api from '@api-platform/api-doc-parser/lib/Api';
+import { createMuiTheme } from "@material-ui/core/styles";
 
 const theme = createMuiTheme({
   palette: {
     primary: {
-      main: '#38a9b4',
-      contrastText: '#fff',
+      main: "#38a9b4",
+      contrastText: "#fff"
     },
     secondary: {
-      main: '#288690',
-    },
-  },
+      main: "#288690"
+    }
+  }
 });
 
-class AdminBuilder extends Component {
-  state = { api: null };
+const AdminBuilder = props => (
+  <Query type="INTROSPECT">
+    {({ data, loading, error }) => {
+      if (loading) {
+        return (
+          <TranslationProvider>
+            <Loading />
+          </TranslationProvider>
+        );
+      }
+      if (error) {
+        console.error(error);
+        return <div>Error while reading the API schema</div>;
+      }
+      return (
+        <Admin theme={theme} {...props}>
+          {data.resources.map(resource => (
+            <Resource
+              name={resource.name}
+              key={resource.name}
+              list={ListGuesser}
+              edit={EditGuesser}
+            />
+          ))}
+        </Admin>
+      );
+    }}
+  </Query>
+);
 
-  componentDidMount() {
-    this.props.dataProvider("INTROSPECT").then(({ data }) => {
-      this.setState({ api: data });
-    });
-  }
-
-  render() {
-    // FIXME: dump Admin Resource code to console
-    const { dataProvider, apiDataProvider, listFieldFilter, ...rest } = this.props;
-    return this.state.api ? (
-      <Admin dataProvider={apiDataProvider} theme={theme} {...rest}>
-        {this.state.api.resources.map(resource => (
-          <Resource
-            name={resource.name}
-            key={resource.name}
-            list={ListGuesser}
-            edit={EditGuesser}
-          />
-        ))}
-      </Admin>
-    ) : (
-      <TranslationProvider>
-        <Loading />
-      </TranslationProvider>
-    );
-  }
-}
-
-export default withDataProvider(AdminBuilder);
+export default AdminBuilder;
 
 AdminBuilder.propTypes = {
   api: PropTypes.instanceOf(Api).isRequired,
