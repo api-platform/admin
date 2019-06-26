94b33581ef774e71eb9fbe18269411bf1c90cce8
JF <jean-francois.les-tilleuls.coop>
2019-06-26 11:43:19 +0200

    ListGuesser

---
+++ src/ListGuesser.js
@@ -0,0 +1,63 @@
+import React, {Children} from 'react';
+import PropTypes from 'prop-types';
+import {
+  Datagrid,
+  List,
+  Query
+} from 'react-admin';
+import fieldFactory from './fieldFactory';
+import {getResource} from './docsUtils';
+
+const getFields = ({ fields }, allowedFieldNames = fields.map(defaultField => defaultField.name)) => fields.filter(({ name }) => allowedFieldNames.includes(name));
+
+const ListGuesser = ({...props}) => {
+  const children = Children.toArray(props.children);
+  const {resource: resourceName, fields: allowedFieldNames} = props;
+
+  return (
+    <Query type="INTROSPECT">
+      {
+        ({ data: api, loading, error }) => {
+          if (loading) {
+            return <div>LOADING</div>;
+          }
+
+          if (error) {
+            console.log(error);
+
+            return <div>ERROR</div>
+          };
+
+          const resource = getResource(api.resources, resourceName);
+
+          return (
+            <List {...props}>
+              <Datagrid>
+                {
+                  getFields(resource, allowedFieldNames).map(
+                    (field) => {
+                      const child = children.find(({ props: { source }}) => source === field.name);
+                      if (undefined === child) {
+                        return fieldFactory(field, {resource});
+                      }
+
+                      return child;
+                    }
+                  )
+                }
+              </Datagrid>
+            </List>
+          );
+        }
+      }
+    </Query>
+  );
+}
+
+export default ListGuesser;
+
+ListGuesser.propTypes = {
+  children: PropTypes.object,
+  resource: PropTypes.string.isRequired,
+  fields: PropTypes.array,
+};
