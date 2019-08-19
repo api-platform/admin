de6dfda2f7aa1b58ad2041e23b9a3ae5ad41c803
Morgan Auchedé <morgan.auchede@gmail.com>
2019-08-19 18:27:09 +0200

    fix test with an HOC withReactAdminQuery (simplificate testing => alternative to SNAPSHOT)
    update documentation to install `api-platform-admin` & `client` to use it

--- src/EditGuesser.js
+++ src/EditGuesser.js
@@ -1,62 +1,27 @@
-import React, {Fragment} from 'react';
+import React from 'react';
 import PropTypes from 'prop-types';
-import {Edit, SimpleForm, Query, Loading} from 'react-admin';
-import {getResourcePropertiesNames, renderInput} from './helpers';
+import {Edit, SimpleForm} from 'react-admin';
+import InputGuesser from './InputGuesser';
+import WithReactAdminQuery from './withReactAdminQuery';
 
-const EditGuesserView = ({children, inputsNames, ...props}) => (
-  <Edit {...props}>
-    <SimpleForm>
-      {inputsNames.map(inputName => (
-        <Fragment key={inputName}>
-          {renderInput(children, inputName, props.resource)}
-        </Fragment>
-      ))}
-    </SimpleForm>
-  </Edit>
-);
+// useful for testing (we don't need Query)
+const EditGuesserComponent = props => {
+  const {children, fields} = props;
 
-EditGuesserView.propTypes = {
-  children: PropTypes.object,
-  inputsNames: PropTypes.array.isRequired,
+  return (
+    <Edit {...props}>
+      <SimpleForm>
+        {children}
+        {fields.map(field => (
+          <InputGuesser key={field.name} source={field.name} />
+        ))}
+      </SimpleForm>
+    </Edit>
+  );
 };
 
 const EditGuesser = props => (
-  <Query type="INTROSPECT">
-    {({data, loading, error}) => {
-      if (loading) {
-        return <Loading />;
-      }
-
-      if (error) {
-        console.error(error);
-        return <div>Error while reading the API schema</div>;
-      }
-
-      const {
-        resource: resourceName,
-        inputs: allowedInputNames,
-        children,
-      } = props;
-
-      const resource = data.resources.find(({name}) => resourceName === name);
-
-      if (!resource || !resource.writableFields) {
-        console.error(
-          `Resource ${resourceName} not present inside api description`,
-        );
-        return `<div>Resource ${resourceName} not present inside api description</div>`;
-      }
-
-      const inputsNames = getResourcePropertiesNames(
-        resource,
-        'writable',
-        allowedInputNames,
-        children,
-      );
-
-      return <EditGuesserView {...props} inputsNames={inputsNames} />;
-    }}
-  </Query>
+  <WithReactAdminQuery component={EditGuesserComponent} {...props} />
 );
 
 export default EditGuesser;
--- src/ShowGuesser.js
+++ src/ShowGuesser.js
@@ -1,63 +1,27 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import {Query, Show, Loading, SimpleShowLayout} from 'react-admin';
-import {getResourcePropertiesNames, renderField} from './helpers';
+import {Show, SimpleShowLayout} from 'react-admin';
+import FieldGuesser from './FieldGuesser';
+import WithReactAdminQuery from './withReactAdminQuery';
 
-const ShowGuesserView = ({children, fieldsNames, ...props}) => (
-  <Show {...props}>
-    <SimpleShowLayout>
-      {fieldsNames.map(fieldName =>
-        renderField(children, fieldName, props.resource, {
-          ...props,
-          key: props,
-        }),
-      )}
-    </SimpleShowLayout>
-  </Show>
-);
+// useful for testing (we don't need Query)
+export const ShowGuesserComponent = props => {
+  const {children, fields} = props;
 
-ShowGuesserView.propTypes = {
-  children: PropTypes.object,
-  fieldsNames: PropTypes.array.isRequired,
+  return (
+    <Show {...props}>
+      <SimpleShowLayout>
+        {children}
+        {fields.map(field => (
+          <FieldGuesser key={field.name} source={field.name} addLabel={true} />
+        ))}
+      </SimpleShowLayout>
+    </Show>
+  );
 };
 
 const ShowGuesser = props => (
-  <Query type="INTROSPECT" resource={props.resource}>
-    {({data, loading, error}) => {
-      if (loading) {
-        return <Loading />;
-      }
-
-      if (error) {
-        console.error(error);
-        return <div>Error while reading the API schema</div>;
-      }
-
-      const {
-        resource: resourceName,
-        fields: allowedFieldNames,
-        children,
-      } = props;
-
-      const resource = data.resources.find(({name}) => resourceName === name);
-
-      if (!resource || !resource.readableFields) {
-        console.error(
-          `Resource ${resourceName} not present inside api description`,
-        );
-        return `<div>Resource ${resourceName} not present inside api description</div>`;
-      }
-
-      const fieldsNames = getResourcePropertiesNames(
-        resource,
-        'readable',
-        allowedFieldNames,
-        children,
-      );
-
-      return <ShowGuesserView {...props} fieldsNames={fieldsNames} />;
-    }}
-  </Query>
+  <WithReactAdminQuery component={ShowGuesserComponent} {...props} />
 );
 
 export default ShowGuesser;
