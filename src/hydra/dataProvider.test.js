735cdbbdd743e22c5d3d7d88b89b201f4498c782
Alan Poulain <contact@alanpoulain.eu>
2019-10-30 16:23:23 +0100

    Fix nested search filters and search filter for collection

--- src/hydra/dataProvider.test.js
+++ src/hydra/dataProvider.test.js
@@ -1,6 +1,9 @@
-import {transformJsonLdDocumentToReactAdminDocument} from './dataProvider';
+import dataProvider, {
+  transformJsonLdDocumentToReactAdminDocument,
+} from './dataProvider';
+import {GET_LIST} from 'react-admin';
 
-describe('map a json-ld document to an admin on rest compatible document', () => {
+describe('Transform a JSON-LD document to a React Admin compatible document', () => {
   const JSON_LD_DOCUMENT = {
     '@id': '/reviews/327',
     id: 327,
@@ -38,36 +41,73 @@ describe('map a json-ld document to an admin on rest compatible document', () =>
     },
   };
 
-  describe('transform the JSON-LD document in React Admin document', () => {
-    const reactAdminDocument = transformJsonLdDocumentToReactAdminDocument(
-      JSON_LD_DOCUMENT,
+  const reactAdminDocument = transformJsonLdDocumentToReactAdminDocument(
+    JSON_LD_DOCUMENT,
+  );
+
+  test('deep clone the original object', () => {
+    expect(reactAdminDocument).not.toBe(JSON_LD_DOCUMENT);
+    expect(reactAdminDocument.aNestedObject).not.toBe(
+      JSON_LD_DOCUMENT.aNestedObject,
     );
+  });
 
-    test('deep clone the original object', () => {
-      expect(reactAdminDocument).not.toBe(JSON_LD_DOCUMENT);
-      expect(reactAdminDocument.aNestedObject).not.toBe(
-        JSON_LD_DOCUMENT.aNestedObject,
+  test('add an id property equal to the original @id property', () => {
+    expect(reactAdminDocument.id).toBe(JSON_LD_DOCUMENT['@id']);
+  });
+
+  test('preserve the previous id property value in a new originId property', () => {
+    expect(reactAdminDocument.originId).toBe(JSON_LD_DOCUMENT.id);
+  });
+
+  test('an React Admin has a custom toString method', () => {
+    expect(reactAdminDocument.toString()).toBe('[object /reviews/327]');
+  });
+
+  test('transform embedded documents to their IRIs', () => {
+    expect(reactAdminDocument.itemReviewed).toBe('/books/2');
+  });
+
+  test('transform arrays of embedded documents to their IRIs', () => {
+    expect(reactAdminDocument.comment[0]).toBe('/comments/1');
+  });
+});
+
+describe('Transform a React Admin request to an Hydra request', () => {
+  const mockFetchHydra = jest.fn();
+  mockFetchHydra.mockReturnValue({
+    json: {'hydra:member': [], 'hydra:totalItems': 3},
+  });
+  const fetchApi = dataProvider('entrypoint', mockFetchHydra);
+
+  test('React Admin get list with filter parameters', () => {
+    return fetchApi(GET_LIST, 'resource', {
+      pagination: {},
+      sort: {},
+      filter: {
+        simple: 'foo',
+        nested: {param: 'bar'},
+        sub_nested: {sub: {param: true}},
+        array: ['/iri/1', '/iri/2'],
+        nested_array: {nested: ['/nested_iri/1', '/nested_iri/2']},
+      },
+    }).then(() => {
+      const searchParams = Array.from(
+        mockFetchHydra.mock.calls[0][0].searchParams.entries(),
       );
-    });
-
-    test('add an id property equal to the original @id property', () => {
-      expect(reactAdminDocument.id).toBe(JSON_LD_DOCUMENT['@id']);
-    });
-
-    test('preserve the previous id property value in a new originId property', () => {
-      expect(reactAdminDocument.originId).toBe(JSON_LD_DOCUMENT.id);
-    });
-
-    test('an React Admin has a custom toString method', () => {
-      expect(reactAdminDocument.toString()).toBe('[object /reviews/327]');
-    });
-
-    test('transform embedded documents to their IRIs', () => {
-      expect(reactAdminDocument.itemReviewed).toBe('/books/2');
-    });
-
-    test('transform arrays of embedded documents to their IRIs', () => {
-      expect(reactAdminDocument.comment[0]).toBe('/comments/1');
+      expect(searchParams[0]).toEqual(['simple', 'foo']);
+      expect(searchParams[1]).toEqual(['nested.param', 'bar']);
+      expect(searchParams[2]).toEqual(['sub_nested.sub.param', 'true']);
+      expect(searchParams[3]).toEqual(['array[0]', '/iri/1']);
+      expect(searchParams[4]).toEqual(['array[1]', '/iri/2']);
+      expect(searchParams[5]).toEqual([
+        'nested_array.nested[0]',
+        '/nested_iri/1',
+      ]);
+      expect(searchParams[6]).toEqual([
+        'nested_array.nested[1]',
+        '/nested_iri/2',
+      ]);
     });
   });
 });
