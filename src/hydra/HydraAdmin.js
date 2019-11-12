43eb0eee38b9ee07ba8780eb0557a3c70c9f28a0
Grégory Copin <gregcop1@gmail.com>
2019-11-12 17:15:20 +0100

    feat: add redux custom store

--- src/hydra/HydraAdmin.js
+++ src/hydra/HydraAdmin.js
@@ -1,9 +1,11 @@
 import { ConnectedRouter } from 'connected-react-router';
+import { createAdminStore } from 'react-admin';
 import { createHashHistory } from 'history';
+import PropTypes from 'prop-types';
 import React, { useEffect, useState } from "react";
 import { AdminContext, CoreAdminUI, Loading, TranslationProvider} from 'react-admin';
-import PropTypes from 'prop-types';
-import withContext from 'recompose/withContext';
+import { Provider } from 'react-redux';
+import { withContext } from 'recompose';
 import dataProviderFactory from './dataProvider';
 import ResourceGuesser from "../ResourceGuesser";
 
@@ -46,22 +48,32 @@ const HydraAdmin = ({
   }
 
   return (
-    <AdminContext
-      dataProvider={dataProvider}
-      authProvider={authProvider}
-      history={history}
-      i18nProvider={i18nProvider}
-      customReducers={customReducers}
-      customSagas={customSagas}
-      locale={locale}
-      {...rest}
-    >
-     <CoreAdminUI>
-       {resources.map(({name}) => (
-         <ResourceGuesser key={name} name={name} />
-       ))}
-     </CoreAdminUI>
-    </AdminContext>
+    <Provider store={createAdminStore({
+      authProvider,
+      dataProvider,
+      i18nProvider,
+      history,
+      customReducers,
+      customSagas,
+      locale,
+    })}>
+      <AdminContext
+        dataProvider={dataProvider}
+        authProvider={authProvider}
+        history={history}
+        i18nProvider={i18nProvider}
+        customReducers={customReducers}
+        customSagas={customSagas}
+        locale={locale}
+        {...rest}
+      >
+       <CoreAdminUI>
+         {resources.map(({name}) => (
+           <ResourceGuesser key={name} name={name} />
+         ))}
+       </CoreAdminUI>
+      </AdminContext>
+    </Provider>
   );
 };
 
