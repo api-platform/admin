d7bd5a512855587b9b1076d8edc635a0d1baf641
Wim Beck <wim.beck@hotmail.com>
2020-06-08 20:50:59 +0200

    Fix issue #305

--- src/hydra/dataProvider.js
+++ src/hydra/dataProvider.js
@@ -188,7 +188,7 @@ export default (
   const convertReactAdminRequestToHydraRequest = (type, resource, params) => {
     const entrypointUrl = new URL(entrypoint, window.location.href);
     const collectionUrl = new URL(`${entrypoint}/${resource}`, entrypointUrl);
-    const itemUrl = new URL(params.id, entrypointUrl);
+    const itemUrl = new URL(`${entrypoint}/${resource}/${params.id}`, entrypointUrl);
 
     switch (type) {
       case CREATE:
