cb2dd96059ea51e8e9d7d8c80e434427b618f9b7
Alan Poulain <contact@alanpoulain.eu>
2020-12-04 14:52:56 +0100

    fix: propTypes warnings in tests

--- src/ResourceGuesser.test.js
+++ src/ResourceGuesser.test.js
@@ -8,13 +8,15 @@ describe('<ResourceGuesser />', () => {
   test('renders with create', () => {
     const CustomCreate = () => {};
 
-    const tree = renderer.render(<ResourceGuesser create={CustomCreate} />);
+    const tree = renderer.render(
+      <ResourceGuesser name="dummy" create={CustomCreate} />,
+    );
 
     expect(tree).toMatchSnapshot();
   });
 
   test('renders without create', () => {
-    const tree = renderer.render(<ResourceGuesser />);
+    const tree = renderer.render(<ResourceGuesser name="dummy" />);
 
     expect(tree).toMatchSnapshot();
   });
@@ -22,13 +24,15 @@ describe('<ResourceGuesser />', () => {
   test('renders with edit', () => {
     const CustomEdit = () => {};
 
-    const tree = renderer.render(<ResourceGuesser edit={CustomEdit} />);
+    const tree = renderer.render(
+      <ResourceGuesser name="dummy" edit={CustomEdit} />,
+    );
 
     expect(tree).toMatchSnapshot();
   });
 
   test('renders without edit', () => {
-    const tree = renderer.render(<ResourceGuesser />);
+    const tree = renderer.render(<ResourceGuesser name="dummy" />);
 
     expect(tree).toMatchSnapshot();
   });
@@ -36,13 +40,15 @@ describe('<ResourceGuesser />', () => {
   test('renders with list', () => {
     const CustomList = () => {};
 
-    const tree = renderer.render(<ResourceGuesser list={CustomList} />);
+    const tree = renderer.render(
+      <ResourceGuesser name="dummy" list={CustomList} />,
+    );
 
     expect(tree).toMatchSnapshot();
   });
 
   test('renders without list', () => {
-    const tree = renderer.render(<ResourceGuesser />);
+    const tree = renderer.render(<ResourceGuesser name="dummy" />);
 
     expect(tree).toMatchSnapshot();
   });
@@ -50,13 +56,15 @@ describe('<ResourceGuesser />', () => {
   test('renders with show', () => {
     const CustomShow = () => {};
 
-    const tree = renderer.render(<ResourceGuesser show={CustomShow} />);
+    const tree = renderer.render(
+      <ResourceGuesser name="dummy" show={CustomShow} />,
+    );
 
     expect(tree).toMatchSnapshot();
   });
 
   test('renders without show', () => {
-    const tree = renderer.render(<ResourceGuesser />);
+    const tree = renderer.render(<ResourceGuesser name="dummy" />);
 
     expect(tree).toMatchSnapshot();
   });
