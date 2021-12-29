9cad7955a4db47db017da3332230b20d7de97110
fabious <fabious@free.fr>
2021-12-29 18:44:31 +0100

    prettier remove default

--- prettier.config.js
+++ prettier.config.js
@@ -1,11 +1,6 @@
 module.exports = {
-  tabWidth: 2,
-  useTabs: false,
-  semi: true,
   singleQuote: true,
   trailingComma: 'all',
-  bracketSpacing: true,
   bracketSameLine: true,
-  arrowParens: 'always',
   endOfLine: 'auto',
 };
