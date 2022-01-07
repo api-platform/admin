a98af5a8885c954b215f3c4356dae8d3220127ed
Alan Poulain <contact@alanpoulain.eu>
2022-01-07 17:35:40 +0100

    test: replace enzyme by testing-library (#414)

--- src/ShowGuesser.test.js
+++ src/ShowGuesser.test.js
@@ -1,56 +1,110 @@
 import React from 'react';
-import { TextField } from 'react-admin';
-import { shallow } from 'enzyme';
-import FieldGuesser from './FieldGuesser';
+import { DataProviderContext, TextField } from 'react-admin';
+import { renderWithRedux } from 'ra-test';
+import { waitFor } from '@testing-library/react';
+import { Resource } from '@api-platform/api-doc-parser';
+
+import ShowGuesser from './ShowGuesser';
+import SchemaAnalyzerContext from './SchemaAnalyzerContext';
+import introspectReducer from './introspectReducer';
+import schemaAnalyzer from './hydra/schemaAnalyzer';
 
 import { API_FIELDS_DATA } from './__fixtures__/parsedData';
-import { IntrospectedShowGuesser } from './ShowGuesser';
 
-jest.mock('./useMercureSubscription');
+const hydraSchemaAnalyzer = schemaAnalyzer();
+const dataProvider = {
+  getOne: () =>
+    Promise.resolve({
+      data: {
+        id: '/users/123',
+        fieldA: 'fieldA value',
+        fieldB: 'fieldB value',
+        deprecatedField: 'deprecatedField value',
+        title: 'Title',
+        body: 'Body',
+      },
+    }),
+  introspect: () =>
+    Promise.resolve({
+      data: {
+        resources: [
+          new Resource('user', '/users', {
+            fields: API_FIELDS_DATA,
+            readableFields: API_FIELDS_DATA,
+            writableFields: API_FIELDS_DATA,
+            parameters: [],
+          }),
+        ],
+      },
+    }),
+  subscribe: () => Promise.resolve({ data: null }),
+  unsubscribe: () => Promise.resolve({ data: null }),
+};
 
 describe('<ShowGuesser />', () => {
-  // FIXME
-  test.skip('renders with no children', () => {
-    const wrapper = shallow(
-      <IntrospectedShowGuesser
-        resource="user"
-        schema={{ name: 'users', title: 'User' }}
-        readableFields={API_FIELDS_DATA}
-        id="ShowComponentId"
-      />,
+  test('renders with no children', async () => {
+    const { queryAllByText } = renderWithRedux(
+      <DataProviderContext.Provider value={dataProvider}>
+        <SchemaAnalyzerContext.Provider value={hydraSchemaAnalyzer}>
+          <ShowGuesser basePath="/users" resource="user" id="/users/123" />
+        </SchemaAnalyzerContext.Provider>
+      </DataProviderContext.Provider>,
+      {
+        admin: {
+          resources: {
+            user: {
+              data: {},
+            },
+          },
+        },
+      },
+      {},
+      { introspect: introspectReducer },
     );
 
-    expect(wrapper).toContainReact(
-      <FieldGuesser source="fieldA" addLabel={true} />,
-    );
-    expect(wrapper).toContainReact(
-      <FieldGuesser source="fieldB" addLabel={true} />,
-    );
-    expect(wrapper).toContainReact(
-      <FieldGuesser source="deprecatedField" addLabel={true} />,
-    );
+    await waitFor(() => {
+      expect(queryAllByText('resources.user.fields.fieldA')).toHaveLength(1);
+      expect(queryAllByText('fieldA value')).toHaveLength(1);
+      expect(queryAllByText('resources.user.fields.fieldB')).toHaveLength(1);
+      expect(queryAllByText('fieldB value')).toHaveLength(1);
+      expect(
+        queryAllByText('resources.user.fields.deprecatedField'),
+      ).toHaveLength(0);
+      expect(queryAllByText('deprecatedField value')).toHaveLength(0);
+    });
   });
 
-  test('renders with custom fields', () => {
-    const wrapper = shallow(
-      <IntrospectedShowGuesser
-        resource="user"
-        readableFields={API_FIELDS_DATA}
-        id="ShowComponentId">
-        <TextField source="id" label={'label of id'} />
-        <TextField source="title" label={'label of title'} />
-        <TextField source="body" label={'label of body'} />
-      </IntrospectedShowGuesser>,
+  test('renders with custom fields', async () => {
+    const { queryAllByText } = renderWithRedux(
+      <DataProviderContext.Provider value={dataProvider}>
+        <SchemaAnalyzerContext.Provider value={hydraSchemaAnalyzer}>
+          <ShowGuesser basePath="/users" resource="user" id="/users/123">
+            <TextField source="id" label={'label of id'} />
+            <TextField source="title" label={'label of title'} />
+            <TextField source="body" label={'label of body'} />
+          </ShowGuesser>
+        </SchemaAnalyzerContext.Provider>
+      </DataProviderContext.Provider>,
+      {
+        admin: {
+          resources: {
+            user: {
+              data: {},
+            },
+          },
+        },
+      },
+      {},
+      { introspect: introspectReducer },
     );
 
-    expect(wrapper).toContainReact(
-      <TextField source="id" label={'label of id'} />,
-    );
-    expect(wrapper).toContainReact(
-      <TextField source="title" label={'label of title'} />,
-    );
-    expect(wrapper).toContainReact(
-      <TextField source="body" label={'label of body'} />,
-    );
+    await waitFor(() => {
+      expect(queryAllByText('label of id')).toHaveLength(1);
+      expect(queryAllByText('/users/123')).toHaveLength(1);
+      expect(queryAllByText('label of title')).toHaveLength(1);
+      expect(queryAllByText('Title')).toHaveLength(1);
+      expect(queryAllByText('label of body')).toHaveLength(1);
+      expect(queryAllByText('Body')).toHaveLength(1);
+    });
   });
 });
--- src/setupTests.js
+++
@@ -1,5 +0,0 @@
-import { configure } from 'enzyme';
-import Adapter from 'enzyme-adapter-react-16';
-import 'jest-enzyme';
-
-configure({ adapter: new Adapter() });
