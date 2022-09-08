3ee34114b52d66dc4bc3dcf468b46909654901be
Paweł Suwiński <dracono@wp.pl>
2022-09-08 15:08:19 +0200

    fix: match https schema.org in getFieldNameFromSchema (#466)

--- src/hydra/schemaAnalyzer.ts
+++ src/hydra/schemaAnalyzer.ts
@@ -18,7 +18,8 @@ const getFieldNameFromSchema = (schema: Resource) => {
   }
 
   const field = schema.fields.find(
-    (schemaField) => schemaField.id === 'http://schema.org/name',
+    (schemaField) =>
+      schemaField.id?.match(/^https?:\/\/schema.org\/name$/) !== null,
   );
 
   return field ? field.name : 'id';
