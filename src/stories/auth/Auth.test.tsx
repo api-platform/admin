c288451c11894f6cf6da1cd098a638a96561932f
adrien guernier <adrien@marmelab.com>
2024-03-26 15:52:38 +0100

    fix: Auth test did not use correct endpoint

--- src/stories/auth/Auth.test.tsx
+++ src/stories/auth/Auth.test.tsx
@@ -2,14 +2,13 @@ import React from 'react';
 import { composeStories } from '@storybook/react';
 import userEvent from '@testing-library/user-event';
 import { render, screen } from '@testing-library/react';
-import fetch from 'node-fetch';
 import * as story from './Auth.stories'; // Change the path to the correct file
 
 const { Basic } = composeStories(story);
 
 describe('stories/auth/Admin', () => {
   it('should render the basic story', async () => {
-    render(<Basic entrypoint="https://localhost/" />);
+    render(<Basic entrypoint="http://php/" />);
     const submit = await screen.findByText('Sign in');
     const username = await screen.findByLabelText('Username *');
     await userEvent.type(username, 'john');
