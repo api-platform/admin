bb524d8cd831abf977e0c3b671c89989a04d821d
adrien guernier <adrien@marmelab.com>
2024-03-27 17:45:47 +0100

    fix: expect is declared but its value is never read in check build

--- src/stories/auth/Auth.stories.ts
+++ src/stories/auth/Auth.stories.ts
@@ -1,5 +1,5 @@
 import type { Meta, StoryObj } from '@storybook/react';
-import { expect, userEvent, within } from '@storybook/test';
+import { userEvent, within } from '@storybook/test';
 
 import Admin from './Admin';
 
