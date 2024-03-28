140834e945390504cbdeb30041be682f610754ac
adrien guernier <adrien@marmelab.com>
2024-03-28 14:29:30 +0100

    chore: remove useless type

--- .storybook/test-runner-jest.ts
+++ .storybook/test-runner-jest.ts
@@ -1,8 +1,5 @@
 import { getJestConfig } from '@storybook/test-runner';
 // The default Jest configuration comes from @storybook/test
-/**
- * @type {import('@jest/types').Config.InitialOptions}
- */
 module.exports = {
   // The default Jest configuration comes from @storybook/test
   ...getJestConfig(),
