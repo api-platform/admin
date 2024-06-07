c2793b22954d5d988e87cb91ab053608eb49efb8
Paweł Suwiński <dracono@wp.pl>
2024-06-07 15:59:51 +0200

    UseReferenceInputControllerParams as outher intersection

--- src/types.ts
+++ src/types.ts
@@ -481,30 +481,26 @@ type InputProps =
   | BooleanInputProps
   | NumberInputProps
   | ArrayInputProps
-  | (Pick<
-      ReferenceArrayInputProps,
-      'filter' | 'page' | 'perPage' | 'sort' | 'enableGetChoices'
-    > &
-      SelectArrayInputProps)
-  | (Pick<
-      ReferenceInputProps,
-      'filter' | 'page' | 'perPage' | 'sort' | 'enableGetChoices'
-    > &
-      SelectInputProps)
   | SelectArrayInputProps
   | SelectInputProps;
 
 export type IntrospectedInputGuesserProps = Partial<InputProps> &
   IntrospectedGuesserProps & {
     transformEnum?: (value: string | number) => string | number;
-  };
+  } & Pick<
+    ReferenceInputProps | ReferenceArrayInputProps,
+    'filter' | 'page' | 'perPage' | 'sort' | 'enableGetChoices'
+  >;
 
 export type InputGuesserProps = Omit<
   InputProps & Omit<BaseIntrospecterProps, 'resource'>,
   'component'
 > & {
   transformEnum?: (value: string | number) => string | number;
-};
+} & Pick<
+    ReferenceInputProps | ReferenceArrayInputProps,
+    'filter' | 'page' | 'perPage' | 'sort' | 'enableGetChoices'
+  >;
 
 export type IntrospecterProps = (
   | CreateGuesserProps
