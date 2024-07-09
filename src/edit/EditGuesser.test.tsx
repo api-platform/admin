bcc26c86b785eca56cf93841bfa838f632629a08
fzaninotto <fzaninotto@gmail.com>
2024-07-09 18:39:16 +0200

    Fix tests

--- src/edit/EditGuesser.test.tsx
+++ src/edit/EditGuesser.test.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 import { AdminContext, FormTab, TextInput } from 'react-admin';
 import { Resource } from '@api-platform/api-doc-parser';
 import { render, screen, waitFor } from '@testing-library/react';
+import '@testing-library/jest-dom';
 import userEvent from '@testing-library/user-event';
 
 import EditGuesser from './EditGuesser.js';
--- src/input/InputGuesser.test.tsx
+++ src/input/InputGuesser.test.tsx
@@ -8,6 +8,7 @@ import {
 } from 'react-admin';
 import { Resource } from '@api-platform/api-doc-parser';
 import { fireEvent, render, screen, waitFor } from '@testing-library/react';
+import '@testing-library/jest-dom';
 import userEvent from '@testing-library/user-event';
 
 import InputGuesser from './InputGuesser.js';
